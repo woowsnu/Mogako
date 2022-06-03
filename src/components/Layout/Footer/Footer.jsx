a0a732329851de09b87a789619337c89ff4dcaf4
woowsnu <woowsnu@gmail.com>
2022-06-03 16:23:00 +0900

    feat: Footer.jsx 작성

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className="footer-logo">
+        <img src="#" alt="logo" />
+      </div>
+      <div className="footer-copy-text">
+        <p>대표: 문선화. 서울특별시 중구 한강대로 416, 서울스퀘어 15층 101호</p>
+        <p>Copyright by (주)카페인. All right reserved.</p>
+        <a href="#">이용약관</a>
+        <a href="#">개인정보처리방침</a>
+      </div>
+      <div className="footer-sns-icon">
+        <img src="#" alt="facebook" />
+        <img src="#" alt="instagram" />
+        <img src="#" alt="twitter" />
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
