29837ec668a45a953e88d885369c431ea5c90568
woowsnu <woowsnu@gmail.com>
2022-06-03 17:10:07 +0900

    feat: CommunityCard.jsx 수정

--- src/components/CommunityCard.jsx
+++ src/components/CommunityCard.jsx
@@ -4,9 +4,15 @@ import styled from "styled-components";
 const CommunityCard = () => {
   return (
     <CardContainer>
-      <h5>모집중</h5>
-      <h3>개발자 없이 SQL로 데이터 추출하고 대시보드 만들기</h3>
-      <p>2월 13일 시작</p>
+      <span>조회수 TOP</span>
+      <span>취업 고민</span>
+      <h3>합격 메일 답장 이렇게 할까요?</h3>
+      <p>고칠 부분이 있는지 봐주시면 감사하겠습니다!</p>
+      <div className="commented-user">
+        <img src="#" alt="avatar" />
+        <p>멘토 12343</p>
+      </div>
+      <button>+더보기</button>
     </CardContainer>
   );
 };
@@ -14,6 +20,8 @@ const CommunityCard = () => {
 export default CommunityCard;
 
 const CardContainer = styled.div`
+    box-sizing: border-box;
+    margin: 0 auto;
     width: 224px;
     height: 280px;
     border-radius: 10px;
