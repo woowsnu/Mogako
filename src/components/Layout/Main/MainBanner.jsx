4933c019873788c4033b3e322d162273ea028587
woowsnu <woowsnu@gmail.com>
2022-06-03 16:48:27 +0900

    feat: Main.jsx에서 메인 베너 컴포넌트로 분리

---
+++ src/components/Layout/Main/MainBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainBanner = () => {
+  return (
+    <section className="main-bg">
+        <h4>개발은 <br />카페인과 함께</h4>
+        <img src="#" />
+        <button>&lt;</button>
+        <button>&gt;</button>
+    </section>
+  );
+};
+
+export default MainBanner;
