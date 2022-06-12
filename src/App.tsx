d3d4c11bbb1629f382759e0a2c92864f1eaf02d9
woowsnu <woowsnu@gmail.com>
2022-06-12 13:07:06 +0900

    chore: BrowserRouter Basename 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { format } from 'path';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home/Home"
 import CampDetail from './pages/CampDetail/CampDetail';
 
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="mogako">
       <App />
     </BrowserRouter>
   </React.StrictMode>
